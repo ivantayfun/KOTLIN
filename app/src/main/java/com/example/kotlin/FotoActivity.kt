200e33a43b5e003d02443713ecda43f8e6ebcad4
Ivan Rybakov <ivan.rubakov85@gmail.com>
2019-09-28 01:05:46 +0300

    28.09.2019 2

---
+++ app/src/main/java/com/example/kotlin/FotoActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kotlin
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_foto.*
+
+class FotoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_foto)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
